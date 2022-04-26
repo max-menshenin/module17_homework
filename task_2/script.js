186c544fd184347cdb7ce676efa557e9cc37ba40
Max Menshenin <max.menshenin.dev@yandex.ru>
2022-04-26 18:02:52 +0300

    init

--- task_2/script.js
+++ task_2/script.js
@@ -1,19 +1,17 @@
 // Сверстайте кнопку, которая будет содержать в себе icon_01 (как в примере в последнем видео).
 // При клике на кнопку иконка должна меняться на icon_02. Повторный клик меняет иконку обратно.
-
 const btn = document.querySelector('.j-btn-test');
-// flag отвечает за смену отрисовки картинки
+const img = document.getElementById('img');
 
 let flag = true;
 
 btn.addEventListener('click', () => {
     if (flag == true) {
-        btn.textContent = 'icon_01.svg';
+        img.src = 'icon_01.svg';
         flag = !flag;
     }
     else {
-        btn.textContent = 'icon_02.svg';
+        img.src = 'icon_02.svg';
         flag = !flag;
     }
 });
-
