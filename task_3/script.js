df0ba8dac23cad81dca4b51e1086b5fd8f873a79
Max Menshenin <max.menshenin.dev@yandex.ru>
2022-04-28 18:00:33 +0300

    not showing grant request dialog

--- task_3/script.js
+++ task_3/script.js
@@ -1,14 +1,18 @@
-/* Задание 3.
+/* Задание 3.
 Сверстайте кнопку, клик на которую будет выводить на экран следующие данные:
-        Размеры экрана пользователя (ширина и высота).
-        Координаты местонахождения пользователя. Если пользователь отказался дать доступ к местоположению 
-        или данная функция недоступна в бразуере, вывести вместо координат сообщение «Информация о местоположении недоступна».*/ 
+Размеры экрана пользователя (ширина и высота).
+Координаты местонахождения пользователя. Если пользователь отказался дать доступ к местоположению 
+или данная функция недоступна в бразуере, вывести вместо координат сообщение «Информация о местоположении недоступна».*/
 
-if ("geolocation" in navigator) {
-    navigator.geolocation.getCurrentPosition((position) => {
-      const { coords } = position;
-      console.log(coords.latitude, coords.longitude);
-    });
-  }
-  
-  
+const btn = document.querySelector('.j-btn-test');
+const status = document.getElementById('status');
+
+btn.addEventListener('click', () => {
+  status.innerText = 'Width: ' + window.innerWidth +"\n"+ 'Height: '+ window.innerHeight;
+    if ("geolocation" in navigator) {
+        /* местоположение доступно */
+    } else {
+        /* местоположение недоступно */
+        alert('denied');
+    }
+});
